71d54ec17a2245fc278485cf59427b236195674f
leerou0827-ctrl <leerou0827@gmail.com>
2026-01-04 00:47:05 +0800

    Add files via upload

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\Users\\User\\Flutter\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Users\\User\\HCI_uthm\\uthm" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\Users\\User\\Flutter\\flutter"
+  "PROJECT_DIR=C:\\Users\\User\\HCI_uthm\\uthm"
+  "FLUTTER_ROOT=C:\\Users\\User\\Flutter\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Users\\User\\HCI_uthm\\uthm\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Users\\User\\HCI_uthm\\uthm"
+  "FLUTTER_TARGET=C:\\Users\\User\\HCI_uthm\\uthm\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9WRVJTSU9OPTMuMzguMw==,RkxVVFRFUl9DSEFOTkVMPXN0YWJsZQ==,RkxVVFRFUl9HSVRfVVJMPWh0dHBzOi8vZ2l0aHViLmNvbS9mbHV0dGVyL2ZsdXR0ZXIuZ2l0,RkxVVFRFUl9GUkFNRVdPUktfUkVWSVNJT049MTkwNzRkMTJmNw==,RkxVVFRFUl9FTkdJTkVfUkVWSVNJT049MTNlNjU4NzI1ZA==,RkxVVFRFUl9EQVJUX1ZFUlNJT049My4xMC4x"
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\Users\\User\\HCI_uthm\\uthm\\.dart_tool\\package_config.json"
+)
